215de3ea253d86c488ab406c44155cb73f04885b
Willem Coetzee <williecoetzee@gmail.com>
2014-07-05 12:09:37 +0200

    Copied solution 6 files over to test.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 var app = {
 
-    initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
-        });
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
         }
+    },
+
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
     }
 
 };
