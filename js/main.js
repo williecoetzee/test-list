4a444b9b72d55f58e447133a264e37e291891ce6
Willem Coetzee <williecoetzee@gmail.com>
2014-07-05 07:45:52 +0200

    Added native style alert message.

--- js/main.js
+++ js/main.js
@@ -14,8 +14,19 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     }
 
 };
