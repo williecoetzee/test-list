523e5d15c8a81f80491bf7f8cd6deec38654f006
Willem Coetzee <williecoetzee@gmail.com>
2014-07-05 11:52:06 +0200

    Improved structure and added styling.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,9 @@
 var app = {
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
     initialize: function() {
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
     },
 
@@ -23,11 +13,6 @@ var app = {
         } else {
             alert(title ? (title + ": " + message) : message);
         }
-    },
-
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
