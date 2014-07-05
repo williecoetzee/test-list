400679bb04b0adf584b6f746ebdd68c3ee184453
Willem Coetzee <williecoetzee@gmail.com>
2014-07-05 08:39:35 +0200

    Add handlebars to the mix.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,20 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
     initialize: function() {
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         var self = this;
         this.store = new MemoryStore(function() {
             self.showAlert('Store Initialized', 'Info');
             self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     showAlert: function (message, title) {
@@ -31,13 +26,7 @@ var app = {
     },
 
     renderHomeView: function() {
-        var html =
-            "<div class='header'><h1>Home</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key'/>" +
-            "<ul class='employee-list'></ul>" +
-            "</div>"
-        $('body').html(html);
+        $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
