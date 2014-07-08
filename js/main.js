236b8506ad1156f3e9e4075232e1870216638447
Willem Coetzee <williecoetzee@gmail.com>
2014-07-08 20:55:49 +0200

    Added blue on click event.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var app = {
         this.store = new MemoryStore(function() {
             $('body').html(new HomeView(self.store).render().el);
         });
+        this.registerEvents();
     },
 
     showAlert: function (message, title) {
@@ -12,6 +13,28 @@ var app = {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
         }
     }
 
